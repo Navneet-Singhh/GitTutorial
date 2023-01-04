79a9dffc571b6d4555d6bee13244f721492ae971
Navneet-Singhh <navneetxperi@gmail.com>
2023-01-04 16:34:01 +0530

    commit 1

---
+++ Maths.java
@@ -0,0 +1,13 @@
+package com.gitPractice;
+
+public class Maths {
+    public void add(){
+        System.out.println("doing the addition");
+        System.out.println("hii");
+    }
+    public static void main(String[] args) {
+        Maths maths=new Maths();
+        maths.add();
+
+    }
+}
